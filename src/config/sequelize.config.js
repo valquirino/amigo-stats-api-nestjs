5992f13ab9cdbc90fbccddd2f14e67cd25363033
valquirino <erca@cesar.school>
2025-07-02 15:21:58 -0300

    feat: add migration system

---
+++ src/config/sequelize.config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres',
+  },
+};
