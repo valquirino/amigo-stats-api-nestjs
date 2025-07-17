6a6f194e43c4f57b89476d06b94b95915e3a10b0
HenriqueSakane <henriquesakane@gmail.com>
2025-07-17 15:30:13 -0300

    refactor: update database configuration to use environment variables for port and add SSL support

--- src/config/sequelize.config.js
+++ src/config/sequelize.config.js
@@ -6,7 +6,7 @@ module.exports = {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
     host: process.env.POSTGRES_HOST,
-    port: 5432,
+    port: process.env.POSTGRES_PORT,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
--- src/shared/infrastructure/database/database.module.ts
+++ src/shared/infrastructure/database/database.module.ts
@@ -6,22 +6,26 @@ import { SequelizeModule } from '@nestjs/sequelize';
   imports: [
     SequelizeModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        // TODO: colocar todas as config no .env e validar as variaveis
-        dialect: 'postgres',
-        host: configService.getOrThrow('POSTGRES_HOST'),
-        port: 5432,
-        username: configService.getOrThrow('POSTGRES_USER'),
-        password: configService.getOrThrow('POSTGRES_PASSWORD'),
-        database: configService.getOrThrow('POSTGRES_DB'),
-        autoLoadModels: true,
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-        },
-      }),
+      useFactory: (configService: ConfigService) => {
+        const isSSL = configService.get('DB_SSL') === 'true';
+        return {
+          dialect: 'postgres',
+          host: configService.getOrThrow('POSTGRES_HOST'),
+          port: configService.getOrThrow('POSTGRES_PORT'),
+          username: configService.getOrThrow('POSTGRES_USER'),
+          password: configService.getOrThrow('POSTGRES_PASSWORD'),
+          database: configService.getOrThrow('POSTGRES_DB'),
+          autoLoadModels: true,
+          dialectOptions: isSSL
+            ? {
+                ssl: {
+                  require: true,
+                  rejectUnauthorized: false,
+                },
+              }
+            : {},
+        };
+      },
       inject: [ConfigService],
     }),
   ],
